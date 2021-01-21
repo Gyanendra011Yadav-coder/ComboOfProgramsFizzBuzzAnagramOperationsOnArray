1e91735cf60bba246e2dc16641fa7fd992118c84
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-01-21 14:32:09 +0530

    GIT INIT
    CREATED THIS CLASS TO WRITE THE CODE OF FIZZ AND BUZZ PROGRAM.

---
+++ src/definationClasses/FizzBuzz.java
@@ -0,0 +1,10 @@
+package definationClasses;/*   Created by IntelliJ IDEA.
+ *   Author: Gyanendra Yadav (Gyanendra011Yadav-coder)
+ *   Date: 1/20/2021
+ *   Time: 3:46 PM
+ *   File: definationClasses.FizzBuzz.java
+ */
+
+public class FizzBuzz {
+}
+
