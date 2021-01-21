1fc0f668433cd73420d3255d5096cb05e5ddccc9
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-01-21 16:37:54 +0530

    INTIALIZE THE VARIABLE isAnagram FOR STORING THE RESULT OF THE ANAGRAM
    AND CHAR FOR STORING THE CHARACTER OF THE name1 SO THAT WE CAN COMPARE IT WITH THE name2 CHARACTER.

--- src/definationClasses/Anagram.java
+++ src/definationClasses/Anagram.java
@@ -11,9 +11,29 @@ import javax.xml.namespace.QName;
 import java.util.Scanner;
 
 public class Anagram {
+    String name1;
+    String name2;
+
+    public Anagram() {
+        this.name1 = "RACE";
+        this.name2 = "ACER";
+    }
+
+    public boolean checkAnagram() {
+        boolean isAngram = false;
+        char c;
+        if (name1.length() == name2.length()) {
+            for (int i = 0; i < name1.length(); i++) {
+                c = name1.charAt(i);
+                isAngram = false;
+
+            }
+        }
+        return isAngram;
 
 
     }
+}
 
 
 
