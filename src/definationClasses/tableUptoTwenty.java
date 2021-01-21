63b55987da0472196e7ee935aef4503ae72060e5
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-01-21 20:52:17 +0530

    GIT INIT
    CREATED THIS CLASS TO EXECUTE THE CODE FOR WRITING THE TABLES

---
+++ src/definationClasses/tableUptoTwenty.java
@@ -0,0 +1,13 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gyanendra Yadav (Gyanendra011Yadav-coder)
+ *   Date: 1/21/2021
+ *   Time: 8:50 PM
+ *   File: tableUptoTwenty.java
+ */
+
+package definationClasses;
+
+public class tableUptoTwenty {
+
+}
+
