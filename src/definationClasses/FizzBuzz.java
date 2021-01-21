2399f4182820cfd297d7b1f75394ecf7061ebb2c
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-01-21 22:58:13 +0530

    WROTE THE STATEMENT TO CHANGE IT FROM THE INTEGER TO STRING.

--- src/definationClasses/FizzBuzz.java
+++ src/definationClasses/FizzBuzz.java
@@ -8,25 +8,30 @@ package definationClasses;/*   Created by IntelliJ IDEA.
 public class FizzBuzz {
     String string;
     int number;
-    public FizzBuzz(){
+
+    public FizzBuzz() {
         this.string = " ";
         this.number = 0;
     }
-    public String fizzBuzz(){
-        for (int index = 0; index <99 ; index++) {
-            if(index % 3 ==0 && index % 5==0) {
+
+    public String fizzBuzz() {
+        for (int index = 0; index < 99; index++) {
+            if (index % 3 == 0 && index % 5 == 0) {
                 string = "FIZZ-BUZZ";
-            }else if (index % 3 == 0) {
-                    string = "FIZZ";
-                } else if (index % 5 == 0) {
-                    string = "BUZZ";
-                } else if (index % 3 != 0 && index % 5 != 0) {
-                    int number = index;
-                }
+            } else if (index % 3 == 0) {
+                string = "FIZZ";
+            } else if (index % 5 == 0) {
+                string = "BUZZ";
+            } else if (index % 3 != 0 && index % 5 != 0) {
+                int number = index;
+                string = Integer.toString(number);
             }
+
+        }
         return string;
+
+
     }
-
-
 }
+
 
