6057122773499e8b3f7029080c16c55396f4f9a9
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-01-21 14:35:11 +0530

    GIT INIT
    CREATED THIS CLASS AS A EXECUTION CLASS TO EXECUTE THE CODE OF OTHER DEFINATION CLASSES.

---
+++ src/MainClass/ExecutionClass.java
@@ -0,0 +1,17 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gyanendra Yadav (Gyanendra011Yadav-coder)
+ *   Date: 1/20/2021
+ *   Time: 3:48 PM
+ *   File: ExecutionClass.java
+ */
+
+package MainClass;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+public class ExecutionClass {
+    public static void main(String[] args) {
+
+    }
+}
+
