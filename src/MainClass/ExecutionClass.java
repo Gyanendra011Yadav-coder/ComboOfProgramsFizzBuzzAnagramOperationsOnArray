ae09fff0792025ef84f5b9c56e98536cd0bf12ec
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-01-21 19:13:35 +0530

    CREATED THE CHOICE VARIABLE TO TAKE THE USER'S CHOICE.

--- src/MainClass/ExecutionClass.java
+++ src/MainClass/ExecutionClass.java
@@ -19,6 +19,8 @@ public class ExecutionClass {
         System.out.println("PRESS::::3---> FOR THE EXECUTION OF THE PROGRAM OF PERFORMING " +
                 "OPERATIONS ON THE ARRAY");
         System.out.println("PRESS::::4--->TO'''''EXIT'''''");
+        int choice;
+
 
     }
 }
