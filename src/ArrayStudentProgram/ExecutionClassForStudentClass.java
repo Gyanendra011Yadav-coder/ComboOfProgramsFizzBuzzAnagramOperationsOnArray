bcfbe8b0bfc82ffb12d291a5149ca7b0e04a68d0
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-01-31 12:07:03 +0530

    git init
    Created the object of the Scanner Class.

---
+++ src/ArrayStudentProgram/ExecutionClassForStudentClass.java
@@ -0,0 +1,17 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gyanendra Yadav (Gyanendra011Yadav-coder)
+ *   Date: 1/31/2021
+ *   Time: 12:05 PM
+ *   File: ExecutionClassForStudentClass.java
+ */
+
+package ArrayStudentProgram;
+
+import java.util.Scanner;
+
+public class ExecutionClassForStudentClass {
+    public static void main(String[] args) {
+        Scanner scannerObject = new Scanner(System.in);
+    }
+}
+
