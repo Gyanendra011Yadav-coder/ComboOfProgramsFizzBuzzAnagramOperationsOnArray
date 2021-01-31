f373c3cda8401bd8cf162df242c41d77247db1c4
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-01-31 12:05:18 +0530

    JUST PASTE THIS CLASS FROM PREVIOUS PACAKAGE TO THIS PACAKAGE.

--- src/ArrayStudentProgram/Student.java
+++ src/ArrayStudentProgram/Student.java
@@ -1,4 +1,4 @@
-/*   Created by IntelliJ IDEA.
+package ArrayStudentProgram;/*   Created by IntelliJ IDEA.
  *   Author: Gyanendra Yadav (Gyanendra011Yadav-coder)
  *   Date: 1/27/2021
  *   Time: 4:20 PM
