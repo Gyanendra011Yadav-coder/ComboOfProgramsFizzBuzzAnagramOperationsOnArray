0a271b0b223d8b9ef15f255ff9f6dc3b35704aaf
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-01-31 11:53:20 +0530

    CREATED THE CLASS NAMED AS STUDENT WHICH WILL RUN THE CODE BY TAKING THE USER'S INPUT AS THERE MARKS IN 4 SUBJECT AND THERE NAME AS WELL AND THEN IT WILL PRINT WHEATHER THE STUDENT IS PASS OR FAIL.

---
+++ src/Student.java
@@ -0,0 +1,30 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gyanendra Yadav (Gyanendra011Yadav-coder)
+ *   Date: 1/27/2021
+ *   Time: 4:20 PM
+ *   File: studentProgram.java
+ */
+
+import java.util.Scanner;
+
+public class Student {
+
+        String studentName;
+        double marksInSubject1;
+        double marksInSubject2;
+        double marksInSubject3;
+        Scanner scannerObject = new Scanner (System.in);
+        public Student(String studentName,double marksInSubject1,double marksInSubject2,double marksInSubject3){
+            this.studentName=studentName;
+            this.marksInSubject1 = marksInSubject1;
+            this.marksInSubject2=marksInSubject2;
+            this.marksInSubject3=marksInSubject3;
+
+
+
+        }
+
+    }
+
+
+
