c8762a22f294101e3d6be12766d035ecd5528064
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-02-03 16:05:12 +0530

    ADDED THE PRINTING STATEMETS.

--- src/RecursionProgram.java
+++ src/RecursionProgram.java
@@ -7,12 +7,14 @@
 
 public class RecursionProgram {
     static void number(int m){
+        System.out.println("THE OUTPUT OF THE TAIL");
         if(m>0){
             System.out.println(m+" ");
             number(m-1);
         }
     }
     static void Head(int g){
+        System.out.println("THE OUTPUT OF THE HEAD.");
         if(g>0) {
             System.out.println(g);
         }
