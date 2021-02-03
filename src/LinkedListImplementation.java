53f4cc649a052df27dd22e190a75eb62d62070e1
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-02-03 16:10:59 +0530

    GIT INIT CRETED THE CLASS NODE.

---
+++ src/LinkedListImplementation.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gyanendra Yadav (Gyanendra011Yadav-coder)
+ *   Date: 2/3/2021
+ *   Time: 4:08 PM
+ *   File: LinkedListImplementation.java
+ */
+
+import org.w3c.dom.Node;
+
+public class LinkedListImplementation {
+//    SINGLY LINKED LIST
+    class Node{
+        int data;
+        Node next;
+
+}
+}
+
