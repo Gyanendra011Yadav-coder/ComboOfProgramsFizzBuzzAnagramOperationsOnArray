eb7cc7e4874a002fbca873dc1e7f55906ec83ff6
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2021-02-22 21:46:38 +0530

    WROTE THE CODE FOR INSERTING THE NEW NODE IN THE LINKED LIST.

--- src/LinkedListPrograms/ReverseTheLinkedListUsingRecursion.java
+++ src/LinkedListPrograms/ReverseTheLinkedListUsingRecursion.java
@@ -4,14 +4,40 @@
  *   Time: 2:26 AM
  *   File: ReverseTheLinkedListUsingRecursion.java
 
+ */
+
 package LinkedListPrograms;
 
-import java.sql.PreparedStatement;
-import java.util.LinkedList;
+import org.w3c.dom.Node;
 
-public class ReverseTheLinkedList {
+class ReverseTheLinkedList {
 
- */
+    static class Node {
+        int data;
+        Node next;
+        public Node(int d){
+            data = d;
+            next = null;
+        }
+
+        static Node reverse(Node head){
+            if(head==null || head.next==null){
+                return head;
+            }else {
+                Node rest = reverse(head.next);
+                head.next.next.next = head;
+                head.next = null;
+            }
+            return reverse(head);
+        }
+
+
+    }
+
+
+}
+
+
 
 
 
--- src/LinkedListPrograms/singlyLinkedListInsertion.java
+++ src/LinkedListPrograms/singlyLinkedListInsertion.java
@@ -7,26 +7,59 @@
 
 package LinkedListPrograms;
 
-public class singlyLinkedListInsertion {
-    Node head;
-   class Node {
-       int data;
-       Node next;
-   }
+import org.w3c.dom.Node;
 
-   public void insert(Node newNode){
-    if(head==null){
-        head = newNode;
-    }else{
-        Node temp = head;
-        while (temp.next!=null){
-            temp =temp.next;
+class LinkedList {
+
+
+     static class Node {
+        int data;
+        Node next;
+         private Node head;
+
+         public Node(int d) {
+            data = d;
+            next = null;
         }
-        temp.next = newNode;
+
+        public void insert(Node newNode) {
+            if (head == null) {
+                head = newNode;
+            } else {
+                Node temp = head;
+                while (temp.next != null) {
+                    temp = temp.next;
+                    temp.next = newNode;
+
+                }
+            }
+        }
+
+        public void printNode() {
+            Node printValue = head;
+            while (printValue.next == null) {
+                System.out.println(printValue.data);
+                printValue = printValue.next;
+            }
+        }
+
+        public static void main(String[] args) {
+            Node node1 = new Node(10);
+            Node node2 = new Node(20);
+            Node node3 = new Node(30);
+
+            node1.next=node2;
+            node2.next= node3;
+            node3.next = null;
+
+            LinkedList obj = new LinkedList();
+
+
+
+        }
     }
-   }
-
-
 
 }
+
+
 
